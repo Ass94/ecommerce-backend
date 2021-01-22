3b1a391f0aba67ef56c66fb5406b6ea6c3b5e3d2
Alex de Souza <alex94tu@gmail.com>
2021-01-22 18:52:41 -0300

    Configurações para o ambiente de desenvolvimento

--- src/main/java/br/com/devpleno/config/DevConfig.java
+++ src/main/java/br/com/devpleno/config/DevConfig.java
@@ -1,6 +1,7 @@
 package br.com.devpleno.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,17 +9,24 @@ import org.springframework.context.annotation.Profile;
 import br.com.devpleno.service.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 	
 	@Autowired
 	private DBService service;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
 	@Bean
 	public boolean initializate() {
+		if(!"create".equals(strategy)) {
+			return false;
+		}
 		service.initializeTestDatabase();
 		return true;
 	}
+	
 	
 
 }
