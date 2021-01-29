fb8b20c27153d4395b65a16b7ff8f5c12e81452f
Alex de Souza <alex94tu@gmail.com>
2021-01-29 09:23:26 -0300

    Salvando cliente

--- src/main/java/br/com/devpleno/resources/ClienteResource.java
+++ src/main/java/br/com/devpleno/resources/ClienteResource.java
@@ -1,15 +1,19 @@
 package br.com.devpleno.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.devpleno.domain.Cliente;
+import br.com.devpleno.dto.ClienteNewDTO;
 import br.com.devpleno.service.ClienteService;
 
 @RestController
@@ -29,6 +33,15 @@ public class ClienteResource {
 	public ResponseEntity<List<Cliente>> findAll() {
 		List<Cliente> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/br/com/devpleno/service/ClienteService.java
+++ src/main/java/br/com/devpleno/service/ClienteService.java
@@ -6,8 +6,14 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.devpleno.domain.Cidade;
 import br.com.devpleno.domain.Cliente;
+import br.com.devpleno.domain.Endereco;
+import br.com.devpleno.domain.enums.TipoCliente;
+import br.com.devpleno.dto.ClienteDTO;
+import br.com.devpleno.dto.ClienteNewDTO;
 import br.com.devpleno.repositories.ClienteRepository;
+import br.com.devpleno.repositories.EnderecoRepository;
 import br.com.devpleno.service.exception.ObjectNotFoundException;
 
 @Service
@@ -15,6 +21,9 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public Cliente findById(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -25,5 +34,31 @@ public class ClienteService {
 	public List<Cliente> findAll() {
 		return repo.findAll();
 	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
 
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cliente = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cidade = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco endereco = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), 
+				objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cidade, cliente);
+		cliente.getEnderecos().add(endereco);
+		cliente.getTelefones().add(objDto.getTelefone1());
+		if(objDto.getTelefone2() != null) {
+			cliente.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3() != null) {
+			cliente.getTelefones().add(objDto.getTelefone3());
+		}
+		return cliente;
+	}
 }
